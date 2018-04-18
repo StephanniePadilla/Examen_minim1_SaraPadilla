eb522245da129a3f94e4ea258235b8864eccda11
steph <stephypadilla08@gmail.com>
2018-04-18 17:57:06 +0200

    Funciones hechas

--- src/test/java/MathManagerTest.java
+++ src/test/java/MathManagerTest.java
@@ -1,2 +1,21 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MathManagerTest {
+    @Before //Junit4
+    public void setUp() throws Exception {
+
+    }
+
+    @After //Junit4
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void realizarOperacion(){
+
+    }
+
 }
--- src/test/java/ReversePolishNotationTest.java
+++ src/test/java/ReversePolishNotationTest.java
@@ -1,2 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ReversePolishNotationTest {
+
+    @Before //Junit4
+    public void setUp() throws Exception {
+
+    }
+
+    @After //Junit4
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void realizarOperacion(){
+
+    }
+
+    @Test
+    public void procesarOperacion(){
+
+    }
+
+
 }
